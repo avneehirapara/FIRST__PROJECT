e4a5c3c7a57e4bd9f1fe207e40139aa07f72d8f0
avaneehirapara <avnihirapara0706@gmail.com>
2022-07-08 10:51:58 +0530

    objects

--- App.js
+++ App.js
@@ -1,69 +1,127 @@
+// import { View, Text } from 'react-native'
+// import React from 'react'
+
+// export default function App() {
+//   let data = [
+//     {
+//       id: 101,
+//       name: 'Abacavir',
+//       quantity: 25,
+//       price: 150,
+//       expiry: 2022,
+//       status: true
+//     },
+//     {
+//       id: 102,
+//       name: 'Eltrombopag',
+//       quantity: 90,
+//       price: 550,
+//       expiry: 2021,
+//       status: true
+//     },
+//     {
+//       id: 103,
+//       name: 'Meloxicam',
+//       quantity: 85,
+//       price: 450,
+//       expiry: 2025,
+//       status: false
+//     },
+//     {
+//       id: 104,
+//       name: 'Allopurinol',
+//       quantity: 50,
+//       price: 600,
+//       expiry: 2023,
+//       status: true
+//     },
+//     {
+//       id: 105,
+//       name: 'Phenytoin',
+//       quantity: 63,
+//       price: 250,
+//       expiry: 2021,
+//       status: false
+//     }
+//   ];
+   
+//   let x = data.filter((d,i)=>(d.expiry>=2022))
+//                .reduce((acc,d,i)=> acc + d.price ,0)
+//   console.log(x);
+
+   
+
+//   return (
+//     <View>
+//       {
+//         data.map((d,i)=>{
+//           return(
+//             <View key={i}>
+//               <Text>{d.name}</Text>
+//               <Text>{d.expiry}</Text>
+//               <Text>{d.price}</Text>
+//             </View>
+//           )
+//         })
+//       }
+//       <Text>total price = {x}</Text>
+//     </View>
+//   )
+// }
+
+
 import { View, Text } from 'react-native'
 import React from 'react'
 
 export default function App() {
-  let data = [
+   Data : [
     {
-      id: 101,
-      name: 'Abacavir',
-      quantity: 25,
-      price: 150,
-      expiry: 2022,
+      name: "amit",
+      age: 35,
+      salary: 40000,
+      bonus: 1000,
       status: true
     },
     {
-      id: 102,
-      name: 'Eltrombopag',
-      quantity: 90,
-      price: 550,
-      expiry: 2021,
-      status: true
-    },
-    {
-      id: 103,
-      name: 'Meloxicam',
-      quantity: 85,
-      price: 450,
-      expiry: 2025,
+      name: "ajay",
+      age: 25,
+      salary: 38000,
+      bonus: 2000,
       status: false
     },
     {
-      id: 104,
-      name: 'Allopurinol',
-      quantity: 50,
-      price: 600,
-      expiry: 2023,
+      name: "mayur",
+      age: 23,
+      salary: 50000,
+      bonus: 500,
       status: true
     },
     {
-      id: 105,
-      name: 'Phenytoin',
-      quantity: 63,
-      price: 250,
-      expiry: 2021,
-      status: false
-    }
+      name: "jay",
+      age: 29,
+      salary: 35000,
+      bonus: 600,
+      status: true
+    },
+    {
+      name: "raj",
+      age: 33,
+      salary: 22000,
+      bonus: 2000,
+      status: true
+    },
   ];
 
-  let x = data.filter((d, i) => (d.expiry >= 2022))
-  // console.log(x);
+  let p = data.filter((d,i)=>(d.status === true))
+  console.log(p);
+  
+   
 
 
 
   return (
     <View>
-      {
-        data.filter((d, i) => {
-          return (
-            <View key={i}>
-              <Text>{d.name}</Text>
-            </View>
-          )
-        })
-      }
-
-
-
+      <Text>App</Text>
     </View>
   )
 }
