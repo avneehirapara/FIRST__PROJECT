d85099418ef8e296189c4d5621f6661b1ee357a9
avaneehirapara <avnihirapara0706@gmail.com>
2022-07-11 10:16:16 +0530

    r

--- App.js
+++ App.js
@@ -3,40 +3,50 @@ import React from 'react'
 
 export default function App() {
 
-  //for objects
+  // //for objects
 
 
-  let arr = [1,2,3,4,5];
+  // let arr = [1,2,3,4,5];
 
-  //copy
-  let Narr = [...arr];
-  console.log(Narr);  // [1,2,3,4,5]
+  // //copy
+  // let Narr = [...arr];
+  // console.log(Narr);  // [1,2,3,4,5]
 
-  // merging
+  // // merging
 
-  let arr1 = [6,7,8,9,10];
-  let merge = [...arr ,...arr1]
-  console.log(merge); //[1,2,3,4,5,6,7,8,9,10]
+  // let arr1 = [6,7,8,9,10];
+  // let merge = [...arr ,...arr1]
+  // console.log(merge); //[1,2,3,4,5,6,7,8,9,10]
 
-  // add  first & last
+  // // add  first & last
 
-  let a = [0,...arr,66];
-  console.log(a); // [0,1,2,3,4,5,66]
+  // let a = [0,...arr,66];
+  // console.log(a); // [0,1,2,3,4,5,66]
 
-  // disturcting
+  // // disturcting
 
-  let [first,second,therd,forth,fiveth] = arr
-  console.log(second); // 2
+  // let [first,second,therd,forth,fiveth] = arr
+  // console.log(second); // 2
 
   //  for objects
 
-  let q = {id:1,name:avnee}
+  let q = {id:1,name:'avnee'}
 
   //copy
 
-  let r = {}
+  let c = {...q}
+  console.log(q); // {id:1,name:'avnee'}
 
+ //maeging
+  let r = {place : 'surat' , age :20}
 
+  let p = {...q, ...r}
+  console.log(p); //{id:1,name:'avnee',place : 'surat' , age :20 }
+
+  //disturcting
+
+  let {first , second , therth , forth  , fifth } = q
+  console.log(second); 
 
 
   return (
