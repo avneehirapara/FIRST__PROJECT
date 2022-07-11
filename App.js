36c0eeb46470c08521038cf91457695a9ff19f7b
avaneehirapara <avnihirapara0706@gmail.com>
2022-07-11 10:06:40 +0530

    r

--- App.js
+++ App.js
@@ -132,55 +132,56 @@
 // }
 
 
-import { View, Text } from 'react-native'
-import React from 'react'
+// import { View, Text } from 'react-native'
+// import React from 'react'
 
-export default function App() {
-   let data =  [
-    {
-      name: "amit",
-      age: 35,
-      salary: 40000,
-      bonus: 1000,
-      status: true
-    },
-    {
-      name: "ajay",
-      age: 25,
-      salary: 38000,
-      bonus: 2000,
-      status: false
-    },
-    {
-      name: "mayur",
-      age: 23,
-      salary: 50000,
-      bonus: 500,
-      status: true
-    },
-    {
-      name: "jay",
-      age: 29,
-      salary: 35000,
-      bonus: 600,
-      status: true
-    },
-    {
-      name: "raj",
-      age: 33,
-      salary: 22000,
-      bonus: 2000,
-      status: true
-    },
-  ];
+// export default function App() {
+//    let data =  [
+//     {
+//       name: "amit",
+//       age: 35,
+//       salary: 40000,
+//       bonus: 1000,
+//       status: true
+//     },
+//     {
+//       name: "ajay",
+//       age: 25,
+//       salary: 38000,
+//       bonus: 2000,
+//       status: false
+//     },
+//     {
+//       name: "mayur",
+//       age: 23,
+//       salary: 50000,
+//       bonus: 500,
+//       status: true
+//     },
+//     {
+//       name: "jay",
+//       age: 29,
+//       salary: 35000,
+//       bonus: 600,
+//       status: true
+//     },
+//     {
+//       name: "raj",
+//       age: 33,
+//       salary: 22000,
+//       bonus: 2000,
+//       status: true
+//     },
+//   ];
 
- let x = data.filter((d,i)=>(d.status === true));
-  let c = data.reduce((acc,d,i)=> acc + d.salary + d.bonus, 0);
-  console.log(x);
+//  let x = data.filter((d,i)=>(d.status === true));
+//   let c = data.reduce((acc,d,i)=> acc + d.salary + d.bonus, 0);
+//   console.log(x);
 
-  return (
-    <View>
-      <Text>total-cost = {c}</Text>
-    </View>
-  )
-}
+//   return (
+//     <View>
+//       <Text>total-cost = {c}</Text>
+//     </View>
+//   )
+// }
+
