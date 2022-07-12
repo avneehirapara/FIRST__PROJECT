b2caf1e6aa1ed8afdb968a799f973cec071c1c1e
avaneehirapara <avnihirapara0706@gmail.com>
2022-07-12 15:11:57 +0530

    r

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-import { View, Text } from 'react-native'
+//------->(Condition: Expiry must be greater or equal 2022 year and status is true)
+
+
+import { StyleSheet,ScrollView, View, Text } from 'react-native'
 import React from 'react'
 
 export default function App() {
@@ -45,35 +48,158 @@ export default function App() {
     }
   ];
    
-  let x = data.filter((d,i)=>(d.expiry>=2022))
+  let x = data.filter((d,i)=>d.expiry>=2022 && d.status === true)
                .reduce((acc,d,i)=> acc + d.price ,0)
                console.log(x);
-               
 
-  
-
-   
-
+  let f = data.filter((d,i)=>d.expiry>=2022 && d.status === true)
+              
   return (
-    <View>
+    <ScrollView style = {StyleSheet.container}>
+    <View style = {styles.container}>
       {
-        data.map((d,i)=>{
+        f.map((d,i)=>{
           let{id ,name,quantity ,price,expiry,status} = d;
 
           return(
             <View key={i}>
-              <Text> name =   {name}</Text>
-              <Text> expiry = {expiry}</Text>
-              <Text> price =  {price}</Text>
-              <Text> id =     {id}</Text>
+              <Text style =  {styles.name}> id =     {id}</Text>
+              <Text  style = {styles.name}> name =   {name}</Text>
+              <Text  style = {styles.id}> quantity ={quantity}</Text>
+              <Text  style = {styles.name}> expiry = {expiry}</Text>
+              <Text  style = {styles.id}> price =  {price}</Text>
+              <Text  style = {styles.name}> status =  {status.toString()}</Text>
             </View>
           )
         })
       }
-      <Text>total price = {x}</Text>
+      <Text  style = {styles.total}>Total price = {x}</Text>
     </View>
+    </ScrollView>
   )
 }
+let styles = StyleSheet.create({
+  container : {
+         backgroundColor : 'black'
+  },
+  name: {
+    color : 'grey',
+    fontSize : 30,
+    fontFamily :'georgia',
+    lineHeight : 90,
+  },
+  total :{
+    color : 'white',
+    fontSize : 30,
+    fontFamily :'georgia',
+    lineHeight : 90,
+  }
+})
+
+
+
+//----->>(Condition: Employee status must be true and give individual sum of salary and bonus, totalCost)
+
+
+// import { StyleSheet, View, ScrollView,SafeAreaView, Text } from 'react-native'
+// import React from 'react'
+
+// export default function App() {
+//    let data =  [
+//     {
+//       name: "amit",
+//       age: 35,
+//       salary: 40000,
+//       bonus: 1000,
+//       status: true
+//     },
+//     {
+//       name: "ajay",
+//       age: 25,
+//       salary: 38000,
+//       bonus: 2000,
+//       status: false
+//     },
+//     {
+//       name: "mayur",
+//       age: 23,
+//       salary: 50000,
+//       bonus: 500,
+//       status: true
+//     },
+//     {
+//       name: "jay",
+//       age: 29,
+//       salary: 35000,
+//       bonus: 600,
+//       status: true
+//     },
+//     {
+//       name: "raj",
+//       age: 33,
+//       salary: 22000,
+//       bonus: 2000,
+//       status: true
+//     },
+//   ];
+
+//     let x = data.filter((d,i)=>d.status === true)
+//                 .reduce((acc,d,i)=>acc + d.salary + d.bonus ,0)
+                 
+//     let f = data.filter((d,i)=>d.status === true)
+
+//   return (
+   
+//     <ScrollView style={styles.container}>
+//         {
+//           f.map((d,i)=>{
+//             let {name , age , salary , bonus , status} = d;
+//             return(
+//               <ScrollView key={i} >
+//                 <Text  style={styles.name} >Name :   {name}</Text>
+//                 <Text style={styles.name} >Age  :   {age}</Text>
+//                 <Text style={styles.name} >Salary : {salary}</Text>
+//                 <Text style={styles.name} >Bonus :  {bonus}</Text>
+//                 <Text style={styles.n1} >-----TOTAL SALARY :{bonus + salary}-----</Text>
+//                 <Text style={styles.name} >Status :{status.toString()}</Text>
+//               </ScrollView>
+//             )
+//           })
+//         }
+//         <Text style={styles.Name} >-----TotalCost = {x}------</Text>
+//     </ScrollView>
+
+//   )
+// }
+// let styles = StyleSheet.create({
+//   container : {
+//     backgroundColor : 'black', 
+   
+    
+//   },
+//   name :{
+//     fontFamily : 'georgia',
+//     color  : '#7C8589',
+//     fontSize : 20,
+//     lineHeight : 35
+    
+//   },
+//   Name :{
+//     color : 'darkgrey',
+//     fontSize : 25,
+//     fontFamily : 'georgia',
+//     lineHeight : 35
+//   },
+//   n1 : {
+//     color :'#CCD5D9',
+//     fontSize : 20,
+//     lineHeight : 35,
+//     fontFamily : 'georgia',
+
+    
+//   }
+// })
+
 
 
 // import { View, Text } from 'react-native'
@@ -119,7 +245,6 @@ export default function App() {
 //   ];
 
   
- 
 
 //   let z = data.filter((d,i)=> d.salary >=35000)
 //           .reduce((acc,d,i)=>acc + d.salary,0);
@@ -133,60 +258,6 @@ export default function App() {
 //     <View>
 //       <Text> salary = {z}</Text>
 //       <Text> bonus = {x}</Text>
-//     </View>
-//   )
-// }
-
-
-// import { View, Text } from 'react-native'
-// import React from 'react'
-
-// export default function App() {
-//    let data =  [
-//     {
-//       name: "amit",
-//       age: 35,
-//       salary: 40000,
-//       bonus: 1000,
-//       status: true
-//     },
-//     {
-//       name: "ajay",
-//       age: 25,
-//       salary: 38000,
-//       bonus: 2000,
-//       status: false
-//     },
-//     {
-//       name: "mayur",
-//       age: 23,
-//       salary: 50000,
-//       bonus: 500,
-//       status: true
-//     },
-//     {
-//       name: "jay",
-//       age: 29,
-//       salary: 35000,
-//       bonus: 600,
-//       status: true
-//     },
-//     {
-//       name: "raj",
-//       age: 33,
-//       salary: 22000,
-//       bonus: 2000,
-//       status: true
-//     },
-//   ];
-
-//  let x = data.filter((d,i)=>(d.status === true));
-//   let c = data.reduce((acc,d,i)=> acc + d.salary + d.bonus, 0);
-//   console.log(x);
-
-//   return (
-//     <View>
-//       <Text>total-cost = {c}</Text>
 //     </View>
 //   )
 // }
